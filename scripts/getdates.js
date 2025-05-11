c972fe2c0036d73c34d1b6890cf3be1a3bc11335
juliusmiguelditan1 <jditan@byupathway.edu>
2025-05-11 23:54:41 +0800

    java

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -2,4 +2,4 @@ const full = document.querySelector("#full");
 
 const today = new Date(document.lastModified);
 
-full.innerHTML = `last modified: <span class="highlight">${today}</span>`;
+full.innerHTML = today;
